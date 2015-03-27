e42bd78bf47ab9913e6169ebd4d7eaf073005bb5
Jinda Lu <q8886888@gmail.com>
2015-03-27 19:54:35 +0800

    广播信息不给源服务器发

--- sync_server/libs/sync.py
+++ sync_server/libs/sync.py
@@ -2,7 +2,7 @@
 #coding:utf8
 # Author          : tuxpy
 # Email           : q8886888@qq.com
-# Last modified   : 2015-03-27 19:17:02
+# Last modified   : 2015-03-27 19:54:11
 # Filename        : libs/sync.py
 # Description     : 
 
@@ -20,8 +20,8 @@ class ServerManager():
 
     def broad_sync(self, server_id, response):
         for server_id in self.server_list:
-#            if server_id == server_id: # 如果广播对象是自己的话，就不广播了
- #               continue
+            if server_id == server_id: # 如果广播对象是自己的话，就不广播了
+                continue
             response['server_id'] =  server_id
             server = self.server_list[server_id]
             server.write_response(response)
