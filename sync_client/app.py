f7397e6ae1dd312ddb2ab43eb6fab7e9e83696e2
Jinda Lu <q8886888@gmail.com>
2015-03-27 20:02:40 +0800

    广播信息不给源服务器发

--- sync_client/app.py
+++ sync_client/app.py
@@ -2,7 +2,7 @@
 #coding:utf8
 # Author          : tuxpy
 # Email           : q8886888@qq.com
-# Last modified   : 2015-03-27 19:46:42
+# Last modified   : 2015-03-27 19:58:08
 # Filename        : app.py
 # Description     : 
 from __future__ import unicode_literals
@@ -39,11 +39,11 @@ class SyncWebSocketApp(WebSocketApp):
             sync_handler.reply_sync()
 
     def print_error(self, error):
-        sys.stderr.write(str(error) + '\n')
+        sys.stderr.write(error + u'\n')
         self.abort_exit = True
 
     def print_info(self, info):
-        sys.stdout.write(info + '\n')
+        sys.stdout.write(info + u'\n')
 
     def on_error(self, ws, error):
         self.print_error(error)
--- sync_server/libs/sync.py
+++ sync_server/libs/sync.py
@@ -2,7 +2,7 @@
 #coding:utf8
 # Author          : tuxpy
 # Email           : q8886888@qq.com
-# Last modified   : 2015-03-27 19:54:11
+# Last modified   : 2015-03-27 20:02:14
 # Filename        : libs/sync.py
 # Description     : 
 
@@ -18,9 +18,9 @@ class ServerManager():
     def server_is_inline(self, server_id):
         return server_id in self.server_list
 
-    def broad_sync(self, server_id, response):
+    def broad_sync(self, from_server_id, response):
         for server_id in self.server_list:
-            if server_id == server_id: # 如果广播对象是自己的话，就不广播了
+            if server_id == from_server_id: # 如果广播对象是自己的话，就不广播了
                 continue
             response['server_id'] =  server_id
             server = self.server_list[server_id]
